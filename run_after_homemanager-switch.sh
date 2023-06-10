ad4371bdb9d761918b7262846865bb3dc788c28c
glen@stella <glen@accur8software.com>
2023-06-10 08:44:24 -0400

    chezmoi ignore for root

--- run_after_homemanager-switch.sh
+++ run_after_homemanager-switch.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-cd ~/.config/home-manager
+if [ $USER != "root" ]; then
+    /home/dev/.nix-profile/bin/mirror-home-manager-profile.py --fource --source /home/dev --target ~
+else
+    cd ~/.config/home-manager
 
-export PATH=$PATH:/nix/var/nix/profiles/default/bin/
+    export PATH=$PATH:/nix/var/nix/profiles/default/bin/
 
-echo "running home-manager switch"
-nix-shell --run "home-manager switch -b backup"
+    echo "running home-manager switch"
+    nix-shell --run "home-manager switch -b backup"
+fi
