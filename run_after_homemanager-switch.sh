4de7ee8b9f996ae8c6420585fb7edb0a0c944e42
glen@stella <glen@accur8software.com>
2023-06-13 10:49:37 -0400

    version bump a8-scripts

--- run_after_homemanager-switch.sh
+++ run_after_homemanager-switch.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [ $USER == "root" ]; then
+    
     if id homemanager >/dev/null 2>&1; then
         echo 'homemanager user exists'
     else
@@ -10,9 +11,16 @@ if [ $USER == "root" ]; then
         adduser --disabled-password --gecos "user for canonical homemanager setup" homemanager
         sudo -u homemanager chezmoi init https://fizzy33@github.com/fizzy33/dotfiles
     fi
+    
+    echo "running chezmoi update on homemanager"
     sudo -u homemanager chezmoi update
+
+    echo "linking homemanager home into root"
     ~homemanager/.nix-profile/bin/mirror-home-manager-profile.py run --force --source ~homemanager --target ~
+    
+    echo "linking tools from nix store into /usr/local/bin"
     ~/.nix-profile/bin/link-nix-tools run
+
 else
     cd ~/.config/home-manager
 
